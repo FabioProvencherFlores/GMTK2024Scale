78865f7b3363fc91197b2ada8039d20126515b67
FabioProvencherFlores <57635847+FabioProvencherFlores@users.noreply.github.com>
2024-08-19 11:12:51 -0400

    ADd speedormeter

--- GMTKScale/Assets/Scripts/Orbit.cs
+++ GMTKScale/Assets/Scripts/Orbit.cs
@@ -1,40 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 
 
 public class Orbit : MonoBehaviour
 {
-	private GameObject _gameObject;
-	private float _currentLerpValue = 0f;
-
 	[SerializeField]
 	[Min(0.001f)]
 	private float secondsPerRotation = 1f;
+
+	[SerializeField, Range(0, 9)]
+	public int speedIdx = 0;
 
 	[SerializeField]
 	[Min(1f)]
 	public float orbitRadius = 1f;
 
-	private void Awake()
-	{
-		_gameObject = gameObject;
-	}
+	public float currentAngle = 0f;
 
-	//private void Update()
-	//{
-	//	UpdateOrbit(Time.deltaTime);
-	//}
+	[Header("Laser Angles")]
+	[SerializeField, Range(1.5f, 2.1f)]
+	float minAngleForLaser = 0f;
+	[SerializeField, Range(1.5f, 2.1f)]
+	float maxAngleForLaser = 0f;
+	[SerializeField]
+	bool debugSkip = false;
+	[SerializeField]
+	public Transform LaserEndPos;
+
+	private void Start()
+	{
+		secondsPerRotation = GameManager.instance.speeds[speedIdx];
+	}
 
 	public void UpdatePosition(float aTime)
 	{
-		float x = orbitRadius * Mathf.Cos(aTime* 6.283f / secondsPerRotation);
-		float z = orbitRadius * Mathf.Sin(aTime* 6.283f / secondsPerRotation);
+		float processedTime = aTime;
+		while (processedTime > secondsPerRotation)
+		{
+			processedTime -= secondsPerRotation;
+		}
+
+		currentAngle = processedTime * 6.28319f / secondsPerRotation;
+
+
+		float x = orbitRadius * Mathf.Cos(currentAngle);
+		float z = orbitRadius * Mathf.Sin(currentAngle);
 		float y = 0f;
 
 
 		gameObject.transform.position = new Vector3(x, y, z);
 	}
 
+	public bool IsInFrontOfLaser()
+	{
+        if (debugSkip) return false;
+        if (currentAngle < minAngleForLaser) return false;
+		if (currentAngle > maxAngleForLaser) return false;
+
+		return true;
+	}
 }
--- GMTKScale/Assets/Scripts/OrbitSimulatorController.cs
+++ GMTKScale/Assets/Scripts/OrbitSimulatorController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [ExecuteInEditMode]
 public class OrbitSimulatorController : MonoBehaviour
@@ -32,6 +33,20 @@ public class OrbitSimulatorController : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI chrono;
 
+    [Header("Speedometer")]
+    [SerializeField]
+    Transform laserStartPos;
+    [SerializeField]
+    Transform laserEndPos;
+    private LineRenderer _lineRenderer;
+    [SerializeField]
+    Color targetedColor;
+    [SerializeField]
+    Color noTargetColor;
+    [SerializeField]
+    bool showLaserIfNoTarget = true;
+    [SerializeField]
+    TextMeshProUGUI speedText;
 
     void Start()
     {
@@ -43,11 +58,19 @@ public class OrbitSimulatorController : MonoBehaviour
 		debugTime = 0f;
 		if (Application.isPlaying)
 			StartTimer();
+
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.positionCount = 2;
+		_lineRenderer.SetPosition(0, laserStartPos.position);
+		_lineRenderer.SetPosition(1, laserEndPos.position);
 	}
 
     // Update is called once per frame
     void Update()
     {
+        bool laserSeesPlanet = false;
+        Vector3 laserTempEndPos = laserEndPos.position;
+        int speedidx = 0;
         foreach (Orbit orbit in orbits)
         {
             float t = 0f;
@@ -62,21 +85,64 @@ public class OrbitSimulatorController : MonoBehaviour
             }
 
             orbit.UpdatePosition(t);
+
+            if (orbit.IsInFrontOfLaser())
+            {
+                if (!laserSeesPlanet)
+                {
+                    laserTempEndPos = orbit.LaserEndPos.position;
+                    speedidx = orbit.speedIdx;
+
+				}
+                laserSeesPlanet = true;
+            }
 		}
 
-		//double mainGameTimerd = (double)GetTimeSinceOpened();
-		//TimeSpan time = TimeSpan.FromSeconds(mainGameTimerd);
-  //      string displayTime = time.Minutes.ToString() + ":" + time.Seconds.ToString() + ":" + time.Milliseconds.ToString(); 
 
-        float timeMS = GetTimeSinceOpened();
-        int timeS = (int)timeMS;
-        string displayTime = "";
-        if (timeS < 100)
-            displayTime += "0";
-        if (timeS < 10)
-            displayTime += "0";
-        displayTime += timeS.ToString();
-        chrono.text = displayTime;
+        UpdateChronoTime();
+
+        if (laserSeesPlanet)
+        {
+		    _lineRenderer.positionCount = 2;
+            _lineRenderer.materials[0].color = targetedColor;
+		    _lineRenderer.SetPosition(0, laserStartPos.position);
+		    _lineRenderer.SetPosition(1, laserTempEndPos);
+            int speed = (10 - speedidx) * 50;
+            string speedtext = "";
+            if (speed < 100) speedtext += "0";
+            speedtext += speed.ToString();
+            speedText.text = speedtext;
+		}
+        else
+        {
+            if (showLaserIfNoTarget)
+            {
+			    _lineRenderer.materials[0].color = noTargetColor;
+			    _lineRenderer.SetPosition(0, laserStartPos.position);
+			    _lineRenderer.SetPosition(1, laserTempEndPos);
+            }
+            else
+            {
+		        _lineRenderer.positionCount = 0;
+            }
+
+            speedText.text = "000";
+        }
+        
+
+	}
+
+    private void UpdateChronoTime()
+    {
+		float timeMS = GetTimeSinceOpened();
+		int timeS = (int)timeMS;
+		string displayTime = "";
+		if (timeS < 100)
+			displayTime += "0";
+		if (timeS < 10)
+			displayTime += "0";
+		displayTime += timeS.ToString();
+		chrono.text = displayTime;
 	}
 
     public float GetTimeSinceOpened()
